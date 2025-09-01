f91345e9d1689a7f73ca4c9a62da56facf21f1b5
Diego Reyes <diegorele13@gmail.com>
2025-09-01 12:02:11 -0500

    feat(router): add cart view route for shopping cart functionality

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
 import ProductDetailView from '../views/ProductDetailView.vue'
+import CartView from '../views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,14 @@ const router = createRouter({
     component: ProductDetailView,
     meta: {
       title: 'Detalle del Producto - La Casa del Encebollado'
+    }
+  },
+  {
+    path: '/carrito',
+    name: 'carrito',
+    component: CartView,
+    meta: {
+      title: 'Mi Carrito - La Casa del Encebollado'
     }
   },
     {
